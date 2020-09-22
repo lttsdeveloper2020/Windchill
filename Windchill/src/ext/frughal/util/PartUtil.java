7e507f46888e2d4c368cadc479db139665ac5ccf
Administrator <Administrator@172.31.0.165>
2020-09-22 11:34:06 +0000

    codebase1

--- Windchill/src/ext/frughal/util/PartUtil.java
+++ Windchill/src/ext/frughal/util/PartUtil.java
@@ -5,7 +5,7 @@ public class PartUtil {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("this is for demo");
+		System.out.println("this is for demo1");
 	}
 
 }
