0d293517db3e863f615d9df499427088c76aede3
Administrator <Administrator@172.31.0.165>
2020-09-22 16:15:58 +0000

    Push the changes

--- Windchill/src/ext/frugal/demoPackage/TestClass3.java
+++ Windchill/src/ext/frugal/demoPackage/TestClass3.java
@@ -1,9 +1,10 @@
 package ext.frugal.demoPackage;
 
-public class TestClass1 {
+public class TestClass3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" AWS Code Deployment");
+
 	}
+
 }
