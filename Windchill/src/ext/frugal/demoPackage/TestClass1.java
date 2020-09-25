6768a8634274fa3ea2543709d7666b9cfb001f8f
Administrator <Administrator@172.31.0.165>
2020-09-25 04:49:10 +0000

    Colud

--- Windchill/src/ext/frugal/demoPackage/TestClass1.java
+++ Windchill/src/ext/frugal/demoPackage/TestClass1.java
@@ -4,6 +4,6 @@ public class TestClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" AWS Code Deployment");
+		System.out.println(" AWS Code Deployment ddd");
 	}
 }
