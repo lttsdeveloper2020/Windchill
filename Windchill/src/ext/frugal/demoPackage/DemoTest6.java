21716b03e3f2bea8c156ce38009741efd8474241
Administrator <Administrator@172.31.0.165>
2020-10-09 13:22:01 +0000

    Testing Push

--- Windchill/src/ext/frugal/demoPackage/DemoTest6.java
+++ Windchill/src/ext/frugal/demoPackage/DemoTest6.java
@@ -4,6 +4,8 @@ public class DemoTest6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println(" Hello ");
 
 	}
 
--- Windchill/src/ext/frugal/demoPackage/TestClass1.java
+++ Windchill/src/ext/frugal/demoPackage/TestClass1.java
@@ -1,9 +1,36 @@
 package ext.frugal.demoPackage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TestClass1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(" AWS Code Deployment ddd");
-	}
+	public static void main(String[] args) throws IOException {
+
+		  //String command = "powershell.exe  your command";
+		  //Getting the version
+		  String command = "powershell.exe  $PSVersionTable.PSVersion";
+		  // Executing the command
+		  Process powerShellProcess = Runtime.getRuntime().exec(command);
+		  // Getting the results
+		  powerShellProcess.getOutputStream().close();
+		  String line;
+		  System.out.println("Standard Output:");
+		  BufferedReader stdout = new BufferedReader(new InputStreamReader(
+		    powerShellProcess.getInputStream()));
+		  while ((line = stdout.readLine()) != null) {
+		   System.out.println(line);
+		  }
+		  stdout.close();
+		  System.out.println("Standard Error:");
+		  BufferedReader stderr = new BufferedReader(new InputStreamReader(
+		    powerShellProcess.getErrorStream()));
+		  while ((line = stderr.readLine()) != null) {
+		   System.out.println(line);
+		  }
+		  stderr.close();
+		  System.out.println("Done");
+
+		 }
 }
