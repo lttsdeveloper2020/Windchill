05bbc2b0b5b52b419b889542c985b5bc034d2c28
Administrator <Administrator@172.31.0.165>
2020-10-12 14:47:58 +0000

    Push

--- Windchill/src/ext/frugal/demoPackage/DemoTest6.java
+++ Windchill/src/ext/frugal/demoPackage/DemoTest6.java
@@ -5,7 +5,7 @@ public class DemoTest6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" Hello ");
+		System.out.println(" Hello Amit ");
 
 	}
 
